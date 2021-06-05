92da0765a388377c93dc89779f38364108f98445
Tim Park <timfpark@gmail.com>
2021-06-04 17:06:25 -0700

    Display ring and cluster

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
+use std::env;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn greet() -> impl Responder {
+    let cluster = env::var("CLUSTER").unwrap_or("unknown".to_string());
+    let ring = env::var("RING").unwrap_or("unknown".to_string());
+
+    format!("Hello from the {} ring in the {} cluster!", &ring, &cluster)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port_string = env::var("PORT").unwrap_or("8080".to_string());
+    let port = port_string.parse::<u16>().unwrap();
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
