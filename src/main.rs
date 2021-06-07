9170172280f07288e1fdd865c50977e24974b622
Tim Park <timfpark@gmail.com>
2021-06-07 16:13:32 -0700

    Parameterize ring and cluster

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 use actix_web::{web, App, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use std::env;
+
 // use std::env;
 
 async fn greet() -> impl Responder {
-    // let cluster = env::var("CLUSTER").unwrap_or("unknown".to_string());
-    // let ring = env::var("RING").unwrap_or("unknown".to_string());
+    let cluster = env::var("CLUSTER").unwrap_or("unknown".to_string());
+    let ring = env::var("RING").unwrap_or("unknown".to_string());
 
-    format!("Hello from the main ring in the azure cluster!") // , &ring, &cluster)
+    format!("Hello from the '{}' ring in the '{}' cluster!", &ring, &cluster)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let port_string = env::var("PORT").unwrap_or("8080".to_string());
-    // let port = port_string.parse::<u16>().unwrap();
+    let port_string = env::var("PORT").unwrap_or("8080".to_string());
+    let port = port_string.parse::<u16>().unwrap();
 
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .route("/", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 80))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
