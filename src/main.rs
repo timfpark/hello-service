6e6c0d64e47e0c78d4a70bc6bf5fae8deebc7ac5
Tim Park <timfpark@gmail.com>
2021-06-07 16:41:18 -0700

    Pare down to just single log line

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
             .route("/", web::get().to(greet))
     })
     .bind(("127.0.0.1", port))?
